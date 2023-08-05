ea17f9f0d9c3f3f7f6bcf312883539ef5cf60ac0
Moktadir Rahman <autocircled@gmail.com>
2023-08-06 00:14:41 +0600

    modele 2 assignment done

--- topic-4.js
+++ topic-4.js
@@ -1,42 +1,7 @@
-/**
- * Calculate a grade based on the given number.
- *
- * @param {number} num - The number to calculate the grade from.
- * @return {string} The calculated grade.
- */
-const gradeCalculator = (num = 0) => {
-    let grade = '';
-    let number = parseInt(num);
-    switch (true) {
-        case (number > 100):
-            grade = 'Wrong Input';
-            break;
-        case number >= 90 && number <= 100:
-            grade = 'A';
-            break;
-        case number >= 80 && number < 90:
-            grade = 'B';
-            break;
-        case number >= 70 && number < 80:
-            grade = 'C';
-            break;
-        case number >= 60 && number < 70:
-            grade = 'D';
-            break;
-        case number >= 0 && number < 60:
-            grade = 'F';
-            break;
-        default:
-            grade = 'Wrong Input';
-            break;
-    }
-
-    return grade.length == 1 ? `Grade: ${grade}` : grade;
+"use strict";
+async function getData() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const body = await response.json();
+    console.log(body);
 }
-
-
-console.log(gradeCalculator(120)); // Wrong Input
-console.log(gradeCalculator(92)); // Grade: A
-console.log(gradeCalculator(75)); // Grade: C
-console.log(gradeCalculator(55)); // Grade: F
-console.log(gradeCalculator(-8)); // Wrong Input
+getData();
