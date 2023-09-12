dfcaba34ffef199ffe3b7b5d331107b5db73188b
Moktadir Rahman <autocircled@gmail.com>
2023-09-12 21:39:54 +0600

    6 - ok

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ import express from "express"
 
 const App = express()
 
-// Set cookies
+// Default route
 App.get("/", (req, res) => {
     res.status(200).send("Welcome to MERN Assignment")
+})
+
+// 404 Route
+App.get("*", (req, res) => {
+    res.status(404).send("404 Not Found")
 })
 
 export default App
