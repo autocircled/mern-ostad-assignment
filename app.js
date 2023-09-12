e06c9ae8e0a16091f2ce5f7efe6309e7395d0922
Moktadir Rahman <autocircled@gmail.com>
2023-09-12 21:30:48 +0600

    1, 2, 4, 5 - ok

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 import express from "express"
 
-const app = express()
+const App = express()
 
 // Set cookies
-app.get("/", (req, res) => {
-    res.cookie("username", "JohnDoe", { maxAge: 1000 * 60 * 60 * 24 })
-    res.cookie("language", "en", { maxAge: 1000 * 60 * 60 * 24 })
-    res.status(200).send("Custom cookie {username, language} has been set")
+App.get("/", (req, res) => {
+    res.status(200).send("Welcome to MERN Assignment")
 })
 
-// Clear cookies
-app.get("/clearcookies", (req, res) => {
-    res.clearCookie("username")
-    res.clearCookie("language")
-    res.status(200).send("Custom cookie {username, language} has been cleared")
-})
-
-app.listen(5000, () => {
-    console.log("Server started on port 5000")
-})
+export default App
